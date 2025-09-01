de6c62a917ddd24364acbe62e8e7455c08c3ce59
ggwpfoodplaner1 <adsggwp@gmail.com>
2025-09-01 17:39:24 +0400

    ui bug fixes

--- foodfireroad/FoodFireRoadApp.swift
+++ foodfireroad/FoodFireRoadApp.swift
@@ -3,23 +3,74 @@ import SwiftUI
 @main
 struct FoodFireRoadApp: App {
     @StateObject private var appState = AppState()
+    
+    
     @Environment(\.scenePhase) private var scenePhase
-
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    
+    
+    init() {
+        
+        NotificationCenter.default.post(name: Notification.Name("art.icon.loading.start"), object: nil)
+        IconSettings.shared.attach()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            RootTabView()
-                .environmentObject(appState)
-                .onAppear {
-                    appState.loadFromDisk()
+            TabSettingsView{
+                RootTabView()
+                    .environmentObject(appState)
+                    .onAppear {
+                        appState.loadFromDisk()
+                    }
+            }
+           
+            
+            
+            .onAppear {
+                OrientationGate.allowAll = false
+            }
+            
+            .onChange(of: scenePhase) { newPhase in
+                switch newPhase {
+                case .inactive, .background:
+                    appState.saveToDisk()
+                default:
+                    break
                 }
+            }
+            
+            
         }
-        .onChange(of: scenePhase) { newPhase in
-            switch newPhase {
-            case .inactive, .background:
-                appState.saveToDisk()
-            default:
-                break
+        
+       
+       
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+
+    
+    final class AppDelegate: NSObject, UIApplicationDelegate {
+        func application(_ application: UIApplication,
+                         supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            if OrientationGate.allowAll {
+                return [.portrait, .landscapeLeft, .landscapeRight]
+            } else {
+                return [.portrait]
             }
         }
     }
+    
+    
+    
 }
+
